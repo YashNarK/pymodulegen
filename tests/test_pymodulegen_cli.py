595b018276ca0d6bdab5fdd8cd923e68a5b40cc3
Narendran A I <narenkrithick@gmail.com>
2023-10-03 14:51:03 +0530

    os independent paths in test cases

--- tests/test_pymodulegen_cli.py
+++ tests/test_pymodulegen_cli.py
@@ -9,7 +9,7 @@ class TestPymodulegenCLI(unittest.TestCase):
         To test the help command
         """
         # Simulate running '__main__.py --help' in the command line
-        result = subprocess.run(['python', 'src\pymodulegen\__main__.py', '--help'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        result = subprocess.run(['python', os.path.join('src', 'pymodulegen', '__main__.py'), '--help'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
         self.assertEqual(result.returncode, 0)  # Check if the command ran successfully
         self.assertIn("usage: __main__.py", result.stdout)  # Check if the expected help text is present
 
@@ -19,7 +19,7 @@ class TestPymodulegenCLI(unittest.TestCase):
         And the content must be empty as the --is_module_only flag is provided
         """
         # Define the command to run
-        cmd = ['python', 'src\pymodulegen\__main__.py', 'some_module_1', '--is_module_only']
+        cmd = ['python', os.path.join('src', 'pymodulegen', '__main__.py'), 'some_module_1', '--is_module_only']
 
         try:
             
@@ -45,10 +45,9 @@ class TestPymodulegenCLI(unittest.TestCase):
         To create a package api in the root directory with __init__.py file.
         And create a module authentication.py under api package and the content must be empty as the --is_module_only flag is provided
         """
-        #print current working directory
-        print(os.getcwd())
+
         # Define the command to run
-        cmd = ['python', 'src\pymodulegen\__main__.py', 'authentication','--directory','api','--is_module_only']
+        cmd = ['python', os.path.join('src', 'pymodulegen', '__main__.py'), 'authentication','--directory','api','--is_module_only']
 
         try:
             
@@ -59,19 +58,19 @@ class TestPymodulegenCLI(unittest.TestCase):
             assert os.path.exists('api'), "Directory 'api' was not created."
 			
             # Check if the file 'api/authentication.py' exists
-            assert os.path.exists('api/authentication.py'), "Module file 'api/authentication.py' was not created."
+            assert os.path.exists(os.path.join('api', 'authentication.py')), "Module file 'api/authentication.py' was not created."
 
             #Check if the file content is empty
-            file_size = os.path.getsize('api/authentication.py')
+            file_size = os.path.getsize(os.path.join('api', 'authentication.py'))
             assert file_size == 0, "Module file 'api/authentication.py' is not empty."
 			
 			# Check if the file 'api/__init__.py' exists
-            assert os.path.exists('api/__init__.py'), "Module file 'api/__init__.py' was not created."
+            assert os.path.exists(os.path.join('api', '__init__.py')), "Module file 'api/__init__.py' was not created."
 
             #Check if the file content is as expected
             directory = (os.getcwd().split(os.sep))[-1]
             expected_content=f"root_path = os.path.join(current_path.split(os.sep + '{directory}')[0], '{directory}')"
-            with open('api/__init__.py','r') as init_file:
+            with open((os.path.join('api', '__init__.py')),'r') as init_file:
                 file_content = init_file.read();
             
             # Perform assertions on the content
@@ -96,7 +95,7 @@ class TestPymodulegenCLI(unittest.TestCase):
         And create a module authentication.py under api package and the content must be having root path sessting as the --not_is_module_only flag is provided
         """
         # Define the command to run
-        cmd = ['python', 'src\pymodulegen\__main__.py', 'authentication','--directory','api','--not_is_module_only']
+        cmd = ['python', os.path.join('src', 'pymodulegen', '__main__.py'), 'authentication','--directory','api','--not_is_module_only']
 
         try:
             
@@ -107,12 +106,12 @@ class TestPymodulegenCLI(unittest.TestCase):
             assert os.path.exists('api'), "Directory 'api' was not created."
             
             # Check if the file 'api/authentication.py' exists
-            assert os.path.exists('api/authentication.py'), "Module file 'api/authentication.py' was not created."
+            assert os.path.exists(os.path.join('api','authentication.py')), "Module file 'api/authentication.py' was not created."
 
             #Check if the file content is as expected
             directory = (os.getcwd().split(os.sep))[-1]
             expected_content=f"root_path = os.path.join(current_path.split(os.sep + '{directory}')[0], '{directory}')"
-            with open('api/authentication.py','r') as init_file:
+            with open(os.path.join('api','authentication.py'),'r') as init_file:
                 file_content = init_file.read();
             
             # Perform assertions on the content
@@ -121,12 +120,12 @@ class TestPymodulegenCLI(unittest.TestCase):
             
             
             # Check if the file 'api/__init__.py' exists
-            assert os.path.exists('api/__init__.py'), "Module file 'api/__init__.py' was not created."
+            assert os.path.exists(os.path.join('api','__init__.py')), "Module file 'api/__init__.py' was not created."
 
             #Check if the file content is as expected
             directory = (os.getcwd().split(os.sep))[-1]
             expected_content=f"root_path = os.path.join(current_path.split(os.sep + '{directory}')[0], '{directory}')"
-            with open('api/__init__.py','r') as init_file:
+            with open(os.path.join('api','__init__.py'),'r') as init_file:
                 file_content = init_file.read();
             
             
